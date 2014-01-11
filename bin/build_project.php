557d14e9cfa74b5d7c1edf23fb967f6a237d853f
Mike Thorn <mthorn@devlucid.com>
2014-01-11 08:30:37 -0500

    initial testing

--- bin/build_project.php
+++ bin/build_project.php
@@ -4,8 +4,11 @@ function lucid__build_project($config)
 {
 	$script = 'cd '.$config['choices']['path'].";\n";
 	echo("Setting up secondary submodules...\n");
+	
+	# the url router
 	$script .= 'git submodule add '.$config['repo-urls']['router-lucid']." lib/lucid-router/;\n";
 	
+	# the UI framework
 	$config['choices']['ui'] = ($config['choices']['ui'] == 'extjs')?'none':$config['choices']['ui'];
 	if($config['choices']['ui'] != 'none')
 	{
@@ -13,6 +16,7 @@ function lucid__build_project($config)
 	}
 	$script .= 'git submodule add '.$config['repo-urls']['validator-lucid']." lib/lucid-validator/;\n";
 	
+	# the orm
 	if($config['choices']['orm'] != 'none')
 	{
 		$script .= 'git submodule add '.$config['repo-urls']['orm-'.$config['choices']['orm']]." lib/".$config['choices']['orm']."-orm/;\n";
@@ -22,6 +26,7 @@ function lucid__build_project($config)
 		$script .= 'git submodule add '.$config['repo-urls']['orm-'.$config['choices']['test']]." lib/".$config['choices']['test']."-test/;\n";
 	}
 	
+	# the next gen css compiler, depending on UI framework
 	if($config['choices']['ui'] == 'bootstrap')
 	{
 		$script .= 'git submodule add '.$config['repo-urls']['less']." lib/less/;\n";
@@ -31,8 +36,15 @@ function lucid__build_project($config)
 		$script .= 'git submodule add '.$config['repo-urls']['scss']." lib/scss/;\n";
 	}
 	
+	# misc javascript libraries
 	$script .= 'git submodule add '.$config['repo-urls']['hash-change']." lib/hash-change/;\n";
 	$script .= 'git submodule add '.$config['repo-urls']['jsmin']." lib/jsmin-php/;\n";
+	
+	if($config['choices']['use-wiki'])
+	{
+		$script .= 'git submodule add '.$config['repo-urls']['project-wiki']." lib/project-wiki/;\n";
+	}
+	
 	$script .= "git submodule update --init --recursive;\n";
 	shell_exec($script);
 	
@@ -42,19 +54,34 @@ function lucid__build_project($config)
 	$script .= 'cp lib/lucid-project/bin/patches.php bin/;';
 	$script .= 'cp lib/lucid-project/bin/test.php bin/;';
 	
+	# generate the serve scripts
 	$serve1 = file_get_contents(__DIR__.'/serve.sh');
 	$serve1 = str_replace('{dev-port}',$config['choices']['dev-port'],$serve1);
+	if($config['choices']['use-wiki'])
+	{
+		$serve1 .= ' && php --server 0.0.0.0:{wiki-port} --docroot $current_dir/../lib/project-wiki/ --php-ini $current_dir/../etc/';
+		$serve1 = str_replace('{wiki-port}',$config['choices']['wiki-port'],$serve1);
+	}
 	file_put_contents($config['choices']['path'].'/bin/serve.sh',$serve1);
 	$serve2 = file_get_contents(__DIR__.'/serve.bat');
 	$serve2 = str_replace('{dev-port}',$config['choices']['dev-port'],$serve2);
+	if($config['choices']['use-wiki'])
+	{
+		$serve2 .= ' && php --server 0.0.0.0:{wiki-port} --docroot $current_dir/../lib/project-wiki/ --php-ini $current_dir/../etc/';
+		$serve2 = str_replace('{wiki-port}',$config['choices']['wiki-port'],$serve2);
+	}
 	file_put_contents($config['choices']['path'].'/bin/serve.bat',$serve2);
 	
+	# move over some other basic files: 
+	#	index.php  = loading skeleton,
+	#	cacher.php = css/less/scss/js minifier/concatenator/compiler/cacher. 
+	#   app.php    = app entry point. Handles json requests.
+	$script .= 'cp lib/lucid-project/www/index.php www/;';
+	$script .= 'cp lib/lucid-project/www/media/cacher.php www/media/;';
+	$script .= 'cp lib/lucid-project/www/app.php www/;';
 	file_put_contents($config['choices']['path'].'/www/media/js/jquery.js',file_get_contents('http://code.jquery.com/jquery-latest.min.js'));
 	
-	$script .= 'cp lib/lucid-project/www/index.php www/;';
-	$script .= 'cp lib/lucid-project/www/app.php www/;';
-	$script .= 'cp lib/lucid-project/www/media/cacher.php www/media/;';
-	
+	# make the directory for less or scss, depending on UI framework choice
 	if($config['choices']['ui'] == 'bootstrap')
 	{
 		$script .= 'cp lib/lucid-project/www/media/less/customizations.less www/media/less;';
@@ -65,7 +92,7 @@ function lucid__build_project($config)
 	}
 	$script .= 'cp lib/lucid-project/etc/cacher.php etc/;';
 	$script .= 'cp lib/lucid-project/etc/db.php etc/;';
-	$script .= 'cp lib/lucid-project/etc/php.ini etc/;';
+	
 
 	echo("Copying over app project files...\n");
 	shell_exec($script);
