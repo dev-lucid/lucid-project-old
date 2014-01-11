7a2e7a4ee2db92dd56bae4abd3f016fcb8bc40dc
Mike Thorn <mthorn@devlucid.com>
2014-01-11 10:21:24 -0500

    initial testing

--- bin/build_project.php
+++ bin/build_project.php
@@ -105,6 +105,7 @@ function lucid__build_project($config)
 	shell_exec($script);
 	
 	# fix up the wiki a bit
+	/*
 	$index = file_get_contents(__DIR__.'/../../project-wiki/index.php');
 	$index = str_replace('WIKI_TITLE = "New wiki"','WIKI_TITLE = "'.$config['choices']['name'].'"',$index);
 	$index = str_replace('LANG = "no"','LANG = "en"',$index);
@@ -112,7 +113,7 @@ function lucid__build_project($config)
 	$index = str_replace('START_PAGE = "Innhold"','START_PAGE = "Landing"',$index);
 	$index = str_replace('TIME_FORMAT = "%d.%m.%Y %R";',"TIME_FORMAT = \"%d.%m.%Y %R\";\n\t\tdate_default_timezone_set('UTC');",$index);
 	file_put_contents(__DIR__.'/../../project-wiki/index.php',$index);
-	
+	*/
 	
 	echo("To run your project, type: ".$config['choices']['last_folder']."/bin/serve.sh\n");
 }
