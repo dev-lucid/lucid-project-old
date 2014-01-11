3693d1d53f59b93fcbdab430ae7082bf07890b4d
Mike Thorn <mthorn@devlucid.com>
2014-01-11 00:17:12 -0500

    initial testing

--- bin/serve.sh
+++ bin/serve.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 current_dir=$(dirname $0)
-php --server 0.0.0.0:{dev-port} --define __STAGE__=dev --docroot $current_dir/../www/ --php-ini $current_dir/../www/
+php --server 0.0.0.0:{dev-port} --docroot $current_dir/../www/ --php-ini $current_dir/../www/
