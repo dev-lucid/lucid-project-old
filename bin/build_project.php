e2e135b75ade74c6519d7b0fc76c2d14e259b20c
Mike Thorn <mike@ifrit.(none)>
2014-03-27 13:52:15 -0400

    fixing path

--- bin/build_project.php
+++ bin/build_project.php
@@ -60,7 +60,7 @@ function lucid__build_project($config)
 	}
 	if($config['choices']['db-use-local-sqlite'] == 1)
 	{
-		$script .= 'cp lib/lucid-project/bin/sqlite-db-build.php bin/db-build.php;';
+		$script .= 'cp lib/lucid-project/bin/sqlite-db-build.sh bin/db-build.sh;';
 		$script .= 'cp lib/lucid-project/db/sqlite-build.sql db/build.sql;';
 		$script .= 'cp lib/lucid-project/etc/sqlite-db.php etc/db.php;';
 	}
